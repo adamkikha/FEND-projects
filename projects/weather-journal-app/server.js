a1d281ccaa114aedfcfe49766cfcd0deacc0b911
Adam Kikha <es-adam.kikha2024@alexu.edu.eg>
2022-09-30 00:42:13 +0200

    first version: >fulfills expected behaviour

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -2,18 +2,38 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(require("cors")());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(8000,()=>{
+    console.log("server running"); 
+    console.log(`running on localhost: 8000`);
+});
+
+// GET Route Setup
+app.get("/get",(req,res)=>{res.send(projectData)});
+
+// POST Route Setup
+app.post("/post",(req,res)=>{
+    const data = {
+        temp: req.body.temp,
+        feel: req.body.feel,
+        date: req.body.date
+    }
+    console.log(data);
+    projectData[data.date] = data;
+});
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,71 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = ",us&appid=29777e09d4e87aadf71931e254dd43dc&units=imperial";
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?zip="
+const feeling = document.getElementById("feelings");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+"."+ d.getDate()+"."+ d.getFullYear();
+
+// GET function to fetch api data
+const getData = async (baseUrl,zip,apiKey) =>{
+    const request = await fetch(baseUrl+zip+apiKey);
+    try {
+        // Transform into JSON
+        const data = await request.json();
+        return data;
+    }
+    catch(error){
+        // appropriately handle the error
+        console.log("error", error);
+    }
+}
+
+// POST function to post data to the app
+const postData = async (url,data) =>{
+    const response = await fetch(url, {
+        method: "POST", 
+        credentials: "same-origin", 
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    console.log("here");
+    try {
+        const newData = await response.json();
+        console.log(newData);
+    }catch(error) {
+      console.log("error", error);
+    }
+}
+
+// Function to update Ui dynamically
+const updateUi = async () =>{
+    const request = await fetch("/get");
+    try {
+    // Transform into JSON
+    const allData = await request.json();
+    // Write updated data to DOM elements
+    document.getElementById("temp").innerHTML = 
+    Math.round(allData[newDate].temp)+ " degrees";
+    document.getElementById("content").innerHTML = allData[newDate].feel;
+    document.getElementById("date").innerHTML = allData[newDate].date;
+    }
+    catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+}
+
+// Adding listener to make the appropriate http requests
+document.getElementById("generate").addEventListener("click",() =>{
+    getData(baseUrl,document.getElementById("zip").value,apiKey).then((d) =>{
+        postData("/post",{
+            date: newDate,
+            temp: d.main.temp,
+            feel: feeling.value
+        });
+    }).then(updateUi);
+});
